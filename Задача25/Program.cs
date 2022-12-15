c992c83bbc2bd72278f833c8039fed630206d442
Alex <asdsu@example.com>
2022-12-15 18:56:48 +0300

    Задача25

---
+++ Задача25/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать метод Math.Pow
+Console.WriteLine("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень первого числа: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int i;
+int sum = 1;
+for (i = 1; i <= m; i++)
+{
+    sum = n * sum;
+}
+Console.WriteLine($"Число {n} в степени {m} равно = {sum}");
